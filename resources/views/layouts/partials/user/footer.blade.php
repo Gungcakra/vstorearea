0d11e64f55b9a9c0bfbebca79b7dd16eadf37d73
Gungcakra <agungcakra888@gmail.com>
2025-06-26 23:00:36 +0800

    remove watermark in footer

--- resources/views/layouts/partials/user/footer.blade.php
+++ resources/views/layouts/partials/user/footer.blade.php
@@ -1,5 +1,5 @@
 <div id="kt_app_footer" class="app-footer">
-    <!--begin::Footer container-->
+    {{-- <!--begin::Footer container-->
     <div class="app-container container-fluid d-flex flex-column flex-md-row flex-center flex-md-stack py-3">
         <!--begin::Copyright-->
         <div class="text-dark order-2 order-md-1">
@@ -21,5 +21,5 @@
         </ul>
         <!--end::Menu-->
     </div>
-    <!--end::Footer container-->
+    <!--end::Footer container--> --}}
 </div>
